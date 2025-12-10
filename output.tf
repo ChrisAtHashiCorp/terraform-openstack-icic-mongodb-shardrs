07eb596bd09ad4cd06d2f47e19f64bf524d5462e
Chris Polansky <cpolansky@hashicorp.com>
2025-12-10 22:24:46 +0000

    Fixing outputs

--- output.tf
+++ output.tf
@@ -2,10 +2,6 @@ output "fqdns" {
   value = { for i, k in local.fqdns : k => "${openstack_compute_instance_v2.nodes[i].access_ip_v4}" }
 }
 
-output "user-data" {
-  value = local.user-data
-}
-
 output "cluster_id" {
-  value = "cfg-${random_id.cluster_id.hex}"
+  value = "shrd-${random_id.cluster_id.hex}"
 }
