2fa7b1f67920fb1a1c29e43ac6cc85be648aa011
Chris Polansky <cpolansky@hashicorp.com>
2025-12-10 21:52:50 +0000

    Adding user-data output

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "fqdns" {
   value = { for i, k in local.fqdns : k => "${openstack_compute_instance_v2.nodes[i].access_ip_v4}" }
 }
 
+output "user-data" {
+  value = local.user-data
+}
+
 output "cluster_id" {
   value = "cfg-${random_id.cluster_id.hex}"
 }
